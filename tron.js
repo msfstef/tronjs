2ea7836aa6dcbc741df1cf863a27dd5478ac628f
Stefanos Mousafeiris <msfstef@gmail.com>
2017-01-15 18:26:24 +0000

    Fixed collisions, introduced text
    Special cases of collisions were fixed, end game text was added

--- tron.js
+++ tron.js
@@ -6,7 +6,7 @@ var ctx = canvas.getContext('2d');
 // Controls are up, down, left, right.
 var p1_ctrls = new Array(87,83,65,68);
 var p2_ctrls = new Array(38,40,37,39);
-var spf = 0.3; // Seconds per frame.
+var spf = 0.03; // Seconds per frame.
 
 
 // Credits to: http://tinyurl.com/zptaaed
@@ -32,37 +32,37 @@ var Grid = function(step_size){
 	this.grid = createArray(this.no_hor, this.no_ver);
 }
 
-Grid.prototype.cover = function(i,j) {
+Grid.prototype.cover = function(i,j, color) {
 	this.grid[i][j] = 1;
 	ctx.beginPath();
 	ctx.rect(i*this.step,
 			j*this.step,
 			this.step,
 			this.step);
-	ctx.fillStyle = '#000000';
+	ctx.fillStyle = color;
 	ctx.fill();
 	ctx.closePath();
 }
 
-Grid.prototype.player = function(i,j) {
+Grid.prototype.player = function(i,j, color) {
 	this.grid[i][j] = 2;
 	ctx.beginPath();
 	ctx.rect(i*this.step,
 			j*this.step,
 			this.step,
 			this.step);
-	ctx.fillStyle = '#ff0000';
+	ctx.fillStyle = color;
 	ctx.fill();
 	ctx.closePath();
 }
 
 Grid.prototype.check_coll = function(i,j) {
-	if (this.grid[i][j] === 1 ||
-		this.grid[i][j] === 2 ||
-		i >= this.no_hor ||
+	if (i >= this.no_hor ||
 		i < 0 ||
 		j >= this.no_ver ||
-		j < 0) {
+		j < 0 ||
+		this.grid[i][j] === 1 ||
+		this.grid[i][j] === 2) {
 		return true
 	} else {
 		return false
@@ -77,14 +77,21 @@ Grid.prototype.clean = function(i,j) {
 Grid.prototype.update = function(players) {
 	for (var i=0; i < players.length; i++) {
 		var p = players[i]
+		if (!p.lost){
 		if (this.check_coll(p.pos[0],p.pos[1])){
-			game = false;
-			loser = i;
+			console.log(p.p_no)
+			p.lost = true
 			loser_no += 1
 		} else {
-			this.cover(p.last_pos[0],p.last_pos[1])
-			this.player(p.pos[0],p.pos[1])
+			this.cover(p.last_pos[0],p.last_pos[1],
+						p.trail_color)
+			this.player(p.pos[0],p.pos[1], 
+						p.head_color)
 		}
+		}
+	}
+	if (loser_no >= players.length - 1) {
+		game = false;
 	}
 }
 
@@ -111,7 +118,23 @@ var Player = function(i,j,ctrls,p_no) {
 	this.last_pos = [i,j]
 	this.p_no = p_no
 	this.ctrls = ctrls
-	this.dir = p_no*2 + 1 // Direction up, right, down, left: 0,1,2,3.
+	// Direction up, right, down, left: 0,1,2,3.
+	this.dir = (p_no*2 + 1)%5 
+	this.lost = false
+	if (p_no == 0){
+		this.head_color = 'red'
+		this.trail_color = 'black'
+	} else if (p_no == 1){
+		this.head_color = 'blue'
+		this.trail_color = 'black'
+	} else if (p_no == 2){
+		this.head_color = 'green'
+		this.trail_color = 'black'
+	} else if (p_no == 3){
+		this.head_color = 'purple'
+		this.trail_color = 'black'
+	}
+
 	this.upPress = false;
 	this.downPress = false;
 	this.leftPress = false;
@@ -135,38 +158,54 @@ Player.prototype.update = function() {
 }
 
 
+
+var drawScore = function () {
+	if (loser_no == players.length) {
+		var text = 'Draw!'
+	} else {
+		for (var i=0; i < players.length; i++) {
+			var p = players[i]
+			if (!p.lost) {
+				var text = 'Player ' + (p.p_no + 1) + ' wins!';
+			}
+		}
+	}
+	ctx.font = "90px Comic Sans MS";
+	ctx.fillStyle = "grey";
+	ctx.textAlign = "center";
+	ctx.fillText(text, canvas.width/2, canvas.height/4);
+}
+
+
+
 var keyDown = function(e,p) {
 	if(e.keyCode == p.ctrls[0]) {
         p.upPress = true;
+        p.downPress = false;
+        p.leftPress = false;
+        p.rightPress = false;
     }
     else if(e.keyCode == p.ctrls[1]) {
         p.downPress = true;
+        p.upPress = false;
+        p.leftPress = false;
+        p.rightPress = false;
     } 
     else if(e.keyCode == p.ctrls[2]) {
         p.leftPress = true;
+        p.downPress = false;
+        p.upPress = false;
+        p.rightPress = false;
     }
     else if(e.keyCode == p.ctrls[3]) {
         p.rightPress = true;
-    }
-}
-
-var keyUp = function(e,p) {
-	if(e.keyCode == p.ctrls[0]) {
-        p.upPress = false;
-    }
-    else if(e.keyCode == p.ctrls[1]) {
         p.downPress = false;
-    } 
-    else if(e.keyCode == p.ctrls[2]) {
+        p.upPress = false;
         p.leftPress = false;
-    }
-    else if(e.keyCode == p.ctrls[3]) {
-        p.rightPress = false;
     }
 }
 
 var game = true
-var loser = ''
 var loser_no = 0
 var step = 10
 var game_grid = new Grid(step)
@@ -174,22 +213,21 @@ game_grid.draw();
 var p1 = new Player(parseInt(canvas.width*0.25/step),
 					parseInt(canvas.height*0.5/step),
 					p1_ctrls, 0);
-var p2 = new Player(parseInt(canvas.width*0.75/step),
+var p2 = new Player(parseInt(canvas.width*0.75/step)+1,
 					parseInt(canvas.height*0.5/step),
 					p2_ctrls, 1);
 var players = new Array(p1,p2);
 
 
 document.addEventListener("keydown", function(e){keyDown(e,p1);}, false);
-document.addEventListener("keyup", function(e){keyUp(e,p1);}, false);
 
 document.addEventListener("keydown", function(e){keyDown(e,p2);}, false);
-document.addEventListener("keyup", function(e){keyUp(e,p2);}, false);
 
 var update = function(){
 	for (var i=0; i<players.length; i++) {
-		players[i].update();
-		console.log(players[i].pos[0])
+		if (!players[i].lost){
+			players[i].update();
+		}
 	}
 	game_grid.update(players);
 }
@@ -198,7 +236,11 @@ var update = function(){
 function repeat() {
   	if (game) {
   		update()
+	} else {
+		drawScore()
 	}
+	setTimeout(function(){
   	requestAnimationFrame(repeat);
+  	}, spf*1000);
 }
 repeat();
